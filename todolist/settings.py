a993ebf2d96b4308c7bbf671a177bc3ec9138a8f
DDevaras <59032984+DDevaras@users.noreply.github.com>
2023-01-15 13:51:31 -0800

    Update settings.py

--- todolist/settings.py
+++ todolist/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'dmi78l^+1gzz)5s^!b20nw@j$id)fp+1^vl&@5i4mn&(8&axfd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
